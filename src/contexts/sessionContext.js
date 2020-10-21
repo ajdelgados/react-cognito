d221721f204f5070b9b479cc400caf4476e08098
Arturo J. Delgado S <ajdelgados@gmail.com>
2020-10-21 20:08:36 -0300

    Init project

---
+++ src/contexts/sessionContext.js
@@ -0,0 +1,42 @@
+import React, { createContext, useReducer } from "react";
+
+const SessionContext = createContext();
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "LOAD_USER_INFORMATION": {
+      return {
+        ...state,
+        loading: true,
+      };
+    }
+    case "LOADED_USER_INFORMATION": {
+      return {
+        ...state,
+        loading: false,
+        authenticated: true,
+        payload: action.payload
+
+      };
+    }
+    default:
+      return state;
+  }
+};
+
+const initialState = {
+  loading: false,
+  authenticated: false,
+  payload: {}
+};
+
+const SessionContextProvider = props => {
+  const [state, dispatch] = useReducer(reducer, initialState);
+  return (
+    <SessionContext.Provider value={{ state, dispatch }}>
+      {props.children}
+    </SessionContext.Provider>
+  );
+};
+
+export {SessionContext, SessionContextProvider}
